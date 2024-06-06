4e4500bf1f0c59e4d4eec0e16eda47200b71b663
shashankagrawal2026 <125136506+shashankagrawal2026@users.noreply.github.com>
2024-06-06 16:11:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { connectMongoDb } = require("./connection");
+
+const { logReqRes } = require("./middlewares");
+const userRouter = require("./routes/user");
+const app = express();
+const PORT = 8000;
+// Connection
+connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1");
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes("log.txt"));
+// Routes
+app.use("/api/users", userRouter);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
